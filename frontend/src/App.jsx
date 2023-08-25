cc9281dda09f5f31f4975d1989ea88dce4dc7f03
PedroHSBernardes <133461209+PedroHenriqueSBer@users.noreply.github.com>
2023-08-25 13:02:53 -0300

    Home initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,5 @@ import Header from './components/Header/Header'
 import {Outlet} from 'react-router-dom'
 export default _ =>
     <>
-        <Header/>
         <Outlet/>
     </>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import App from './App'
 import Home from './components/Home/Home'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
-                <Route index element={<Home />}/>
+                <Route index element={
+                    <>
+                        <Header/>
+                        <Home />
+                    </>
+                }/>
             </Route>
         </Routes>
     </BrowserRouter>
